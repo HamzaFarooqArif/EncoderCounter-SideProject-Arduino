cc513398d5f8c51d6be234629aad6d5e08527876
Hamza <m.hamza.arif786@gmail.com>
2020-07-24 06:16:13 +0500

    ToBinary

--- RotatryEncoderCounter/RotatryEncoderCounter.ino
+++ RotatryEncoderCounter/RotatryEncoderCounter.ino
@@ -1,8 +1,132 @@
  #define outputA 12
  #define outputB 11
  #define outputZ 10
- 
- int totalPulse = 2048;
+
+//Encoder Configuration
+int totalPulse = 2048;
+int totalDevices = 10;
+
+//Led pin configuration
+int led1=2;
+int led2=3;
+int led3=4;
+int led4=5;
+
+void toBinaryPins(int val)
+{
+  if(val == 0)
+  {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+  }
+  else if(val == 1)
+  {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+  }
+  else if(val == 2)
+  {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+  }
+  else if(val == 3)
+  {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+  }
+  else if(val == 4)
+  {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, LOW);
+  }
+  else if(val == 5)
+  {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, LOW);
+  }
+  else if(val == 6)
+  {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, LOW);
+  }
+  else if(val == 7)
+  {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, LOW);
+  }
+  else if(val == 8)
+  {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, HIGH);
+  }
+  else if(val == 9)
+  {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, HIGH);
+  }
+  else if(val == 10)
+  {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, HIGH);
+  }
+  else if(val == 11)
+  {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, HIGH);
+  }
+  else if(val == 12)
+  {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+  }
+  else if(val == 13)
+  {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+  }
+  else if(val == 14)
+  {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+  }
+  else if(val == 15)
+  {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+  }
+}
  
  int counter = 0;
  int previousPulseCounter = 0;
@@ -33,6 +157,7 @@
         if(previousPulseCounter != pulseCounter)
         {
           counter++;
+          toBinaryPins(counter);
           Serial.println(counter);
         }
       }
@@ -46,6 +171,7 @@
         if(previousPulseCounter != pulseCounter)
         {
           counter--;
+          toBinaryPins(counter);
           Serial.println(counter);
         }
        }
@@ -59,6 +185,11 @@
    pinMode (outputA,INPUT);
    pinMode (outputB,INPUT);
    pinMode (outputZ,INPUT);
+   pinMode (led1, OUTPUT);
+   pinMode (led2, OUTPUT);
+   pinMode (led3, OUTPUT);
+   pinMode (led4, OUTPUT);
+   pinMode(LED_BUILTIN, OUTPUT);
    
    Serial.begin (115200);
    aLastState = digitalRead(outputA);   
